f24a1414bcca50b93b9e7e02abf12e7690234a5b
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-05 15:38:04 +0530

    Font Family Insert

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+      
+    },
   },
   plugins: [],
 }
