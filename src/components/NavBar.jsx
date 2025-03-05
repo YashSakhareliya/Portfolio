44ad89eda81bfc253d0373ff7ada50b1584db8fb
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-06 01:02:23 +0530

    NavLink with motion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import { forwardRef } from 'react'
+
+const motionNavLink = motion(forwardRef((props, ref) => <NavLink {...props} ref={ref} />))
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+    {/* desktop navbar */}
+    <motion.nav
+    initial={{y: -100}}
+    animate={{y: 0}}
+    transition={{type: 'spring', stiffness: 300, damping: 30}}
+    className='fixed top-0 left-0 right-0 z-50 px-6 py-4 transition-all duration-300'
+    >
+
+    </motion.nav>
+    </>
   )
 }
 
