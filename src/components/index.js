8a9a5072af24326adf98d8ba86a263ee35abdee5
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:38:22 +0530

    Contace Imported

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import About from "./About";
 import Projects from "./Projects";
 import Experience from "./Experience";
 import Blog from "./Blog";
+import Contact from "./Contact";
 
-export { Navbar, Hero, Projects, About, Experience, Blog };
+export { Navbar, Hero, Projects, About, Experience, Blog, Contact };
