72ec5312f3729c681a4a8611bc016d1224cdcb11
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:46:20 +0530

    Contact Heading Text Done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,22 @@ const Contact = () => {
       const [isSubmitting, setIsSubmitting] = useState(false);
       const [isSubmitted, setIsSubmitted] = useState(false);
   return (
-    <div>
-      
-    </div>
+    <section className={`py-20 px-4 ${ isDarkMode ? 'bg-dark-bg-primary text-gray-50' : ' bg-gray-50 text-gray-900'}`}>
+        <div className='container mx-auto'>
+            <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="text-center mb-16"
+            >
+            <h2 className="text-4xl font-bold mb-6">Get in Touch</h2>
+            <p className="max-w-2xl mx-auto text-lg opacity-80">
+                Have a project in mind or want to collaborate? I'd love to hear from you.
+            </p>
+            </motion.div>
+        </div>
+    </section>
   )
 }
 
