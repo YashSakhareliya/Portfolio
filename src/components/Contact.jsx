30b938b13bfafeb28ad4e29e13aaf7a259eec08d
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:41:47 +0530

    Contact Deps Imported

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { motion } from 'framer-motion'
+import { Send, Mail, Phone, MapPin, Github, Linkedin, Twitter, CheckCircle } from 'lucide-react';
 
 const Contact = () => {
+    const isDarkMode =  useSelector((state) => state.theme.darkMode)
   return (
     <div>
       
