534276ef3dd391d8116888a0239758e37f7bbbe9
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:28:44 +0530

    import depedencies

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useSelector } from 'react-redux'
 
 const Blog = () => {
+
+    const isDarkMode = useSelector((state) => state.theme.darkMode)
   return (
     <div>
       
