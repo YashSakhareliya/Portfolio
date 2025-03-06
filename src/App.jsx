3e726034265d7df1bd062b96b2acee9e0451d7ab
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:26:19 +0530

    Blog imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Experience, Navbar } from "./components";
+import { Blog, Experience, Navbar } from "./components";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -32,6 +32,10 @@ function App() {
 
               <section id="experience" className="w-full">
                 <Experience />
+              </section>
+
+              <section className="w-full" id="blog">
+                <Blog />
               </section>
             </main>
           </div>
