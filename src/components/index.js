7edd57432407f29b476b010fc9fe61e9eae40ed8
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:25:14 +0530

    Blog Imported

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import About from "./About";
 import Projects from "./Projects";
 import Experience from "./Experience";
+import Blog from "./Blog";
 
-export { Navbar, Hero, Projects, About, Experience };
+export { Navbar, Hero, Projects, About, Experience, Blog };
