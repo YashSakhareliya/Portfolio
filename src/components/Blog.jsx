af5b779e05345004d591c1a00201cd9320c2ed2c
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-07 00:30:11 +0530

    Blog Category Added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
 
+
+const categories = [
+    'All Posts',
+    'Artificial Intelligence',
+    'React',
+    'Architecture',
+    'Web Development'
+];
 const Blog = () => {
 
     const isDarkMode = useSelector((state) => state.theme.darkMode)
