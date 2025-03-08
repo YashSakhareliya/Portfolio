a9f3ed72c63da908868520a84d92c62a33aa4aec
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-08 10:46:36 +0530

    Preloader export

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import Experience from "./Experience";
 import Blog from "./Blog";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import Preloader from "./Preloader";
 
-export { Navbar, Hero, Projects, About, Experience, Blog, Contact, Footer };
+export { Navbar, Hero, Projects, About, Experience, Blog, Contact, Footer, Preloader };
